61fdc1a56e5e32aa008df5486e4e2c6fd0ef01cb
helluy <helluy@unistra.fr>
2024-03-18 20:15:54 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+// cli tool for using the trsltx library
+// the utility takes two optionnal arguments:
+// - the input filename. By default it is article_fr.tex
+// the _fr suffix is used to indicate the language of the file (here french)
+// - the output filename. By default it is article_en.tex
+// the _en suffix is used to indicate the language of the file (here english) 
+
+// needed includes for handling arguments of the command line
+use std::env;
+
+// list of known languages: en, fr, es, de, it, pt, ru
+
+const LANGUAGES : [(&str,&str); 7] = [
+    ("en", "english"),
+    ("fr", "french"),
+    ("es", "spanish"),
+    ("de", "german"),
+    ("it", "italian"),
+    ("pt", "portuguese"),
+    ("ru", "russian")
+];
+
+
+
+fn main() {
+    let args : Vec<String> = env::args().collect();
+    let input_file = if args.len() > 1 { &args[1] } else { "article_fr.tex" };
+    let output_file = if args.len() > 2 { &args[2] } else { "article_en.tex" };
+
+    let input_file_name = input_file.clone();    
+    let output_file_name = output_file.clone();
+
+
+    // asserts that the filenames end with .tex
+    assert!(input_file.ends_with(".tex"));
+    assert!(output_file.ends_with(".tex"));
+
+    // removes the .tex suffix
+    let input_file = &input_file[..input_file.len()-4];
+    let output_file = &output_file[..output_file.len()-4];
+
+    // split the string at the _
+    let input_file : Vec<&str> = input_file.split('_').collect();
+    let output_file : Vec<&str> = output_file.split('_').collect();
+
+    assert!(input_file.len() == 2);
+    assert!(output_file.len() == 2);
+
+    let input_lang = input_file[1];
+    let output_lang = output_file[1];
+
+    println!("input language: {:?}", input_lang);
+    println!("input file name: {:?}", input_file_name);
+    println!("output language: {:?}", output_lang);
+    println!("output file name: {:?}", output_file_name);
+
+}
