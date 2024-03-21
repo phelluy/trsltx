0bbe5062728b84a1363ca6d70b03825b2214e09e
helluy <helluy@unistra.fr>
2024-03-21 21:40:07 +0100

    up

--- src/lib.rs
+++ src/lib.rs
@@ -129,7 +129,10 @@ impl Trsltx {
         let chunks = toscan.split("%trsltx-split\n");
         for chunk in chunks {
             if chunk.contains("%trsltx-begin-ignore") {
-                assert!(chunk.contains("%trsltx-end-ignore"), "Ignored chunks cannot be split.");
+                assert!(
+                    chunk.contains("%trsltx-end-ignore"),
+                    "Ignored chunks cannot be split."
+                );
                 self.chunks.push((chunk.to_string(), ChunkType::Unchanged));
             } else if chunk.contains("%trsltx-end-ignore") {
                 panic!("Unbalanced %trsltx-end-ignore");
@@ -141,7 +144,7 @@ impl Trsltx {
         let numchunks = self.chunks.len();
 
         for i in 0..numchunks {
-            let (mut s, t) = self.chunks[i].clone();
+            let (s, t) = self.chunks[i].clone();
             let chunk_length = s.len();
             assert!(chunk_length < 1000, "Chunk too long");
             match t {
@@ -185,7 +188,10 @@ impl Trsltx {
         }
         // last cleaning:
         // remove the %trsltx-split immediately following %trsltx-end-ignore
-        body_translated = body_translated.replace("%trsltx-end-ignore\n%trsltx-split\n", "%trsltx-end-ignore\n");
+        body_translated = body_translated.replace(
+            "%trsltx-end-ignore\n%trsltx-split\n",
+            "%trsltx-end-ignore\n",
+        );
         self.body_translated = body_translated;
     }
 
@@ -239,21 +245,10 @@ pub fn get_lang_name(lang: &str) -> String {
     lang.to_string()
 }
 
-/// translate a latex chunk using the textsynth LLM api
-/// the preprompt is in the file "prompt.txt"
-/// the api key is in the file "api_key.txt" or
-/// in the environment variable "TEXTSYNTH_API_KEY"
-fn translate_one_chunk(chunk: &str, input_lang: &str, output_lang: &str) -> String {
-    // get the preprompt
-    let mut prompt = std::fs::read_to_string("src/prompt.txt").expect("cannot read preprompt");
-
-    let input_lang = get_lang_name(input_lang).to_string();
-    let output_lang = get_lang_name(output_lang).to_string();
-
-    // in the prompt, replace <lang_in> by the input language and <lang_out> by the output language
-    prompt = prompt.replace("<lang_in>", input_lang.as_str());
-    prompt = prompt.replace("<lang_out>", output_lang.as_str());
-
+/// one chat operation with the textsynth LLM
+/// send the question
+/// and returns an answer
+fn chat_with_ts(question: &str) -> String {
     // get the api key from the file "api_key.txt" or if the file does not exist, from the environment variable "TEXTSYNTH_API_KEY"
     let api_key = match std::fs::read_to_string("api_key.txt") {
         // if the file exists, get the api key from the file
@@ -269,8 +264,6 @@ fn translate_one_chunk(chunk: &str, input_lang: &str, output_lang: &str) -> Stri
     use serde_json::json;
     use serde_json::Value;
 
-    let question = format!("{}{}", prompt, chunk);
-    //println!("{:?}", question);
     let req = json!({
         "messages": [question],
         "temperature": 0.5,
@@ -287,7 +280,7 @@ fn translate_one_chunk(chunk: &str, input_lang: &str, output_lang: &str) -> Stri
         .expect("Failed to send request")
         .json::<Value>();
 
-    let trs_chunk: String = match res {
+    let answer: String = match res {
         Ok(resp) => {
             let text = resp["text"].as_str().expect("Failed to get text");
             //println!("{:?}", text);
@@ -299,8 +292,119 @@ fn translate_one_chunk(chunk: &str, input_lang: &str, output_lang: &str) -> Stri
         }
     };
 
+    answer
+}
+
+/// one completion operation with the textsynth LLM
+/// send the question and a formal grammar (as Some(String) or None)
+/// and returns an answer
+fn complete_with_ts(prompt: &str, grammar: Option<String>) -> String {
+    // get the api key from the file "api_key.txt" 
+    //or if the file does not exist, from the environment variable "TEXTSYNTH_API_KEY"
+    let api_key = match std::fs::read_to_string("api_key.txt") {
+        // if the file exists, get the api key from the file
+        // removing the spaces and newlines with trim()
+        Ok(api_key) => api_key.trim().to_string(),
+        Err(_) => std::env::var("TEXTSYNTH_API_KEY").expect("You have to provide an api key in the file api_key.txt or by export TEXTSYNTH_API_KEY=api_key"),
+    };
+
+    // call the textsynth REST API
+    let url = "https://api.textsynth.com/v1/engines/mixtral_47B_instruct/completions";
+    let max_tokens = 1000;
+
+    use serde_json::json;
+    use serde_json::Value;
+
+    let req = match grammar {
+        Some(gr) => {
+            json!({
+                "prompt": prompt,
+                "temperature": 0.5,
+                "max_tokens": max_tokens,
+                "grammar": gr
+            })
+        }
+        None => {
+            // println!("No grammar");
+            json!({
+                "prompt": prompt,
+                "temperature": 0.5,
+                "max_tokens": max_tokens
+            })
+        }
+    };
+    // println!("Req= {:?}", req);
+
+    let client = reqwest::blocking::Client::new();
+    let res = client
+        .post(url)
+        .header("Content-Type", "application/json")
+        .header("Authorization", format!("Bearer {}", api_key))
+        .json(&req)
+        .send()
+        .expect("Failed to send request")
+        .json::<Value>();
+
+    let answer: String = match res {
+        Ok(resp) => {
+            println!("{:?}", resp);
+            let text = resp["text"].as_str().expect("Failed to get text");
+            //println!("{:?}", text);
+            text.to_string()
+        }
+        Err(e) => {
+            println!("Request error: {:?}", e);
+            "".to_string()
+        }
+    };
+
+    answer
+}
+
+/// translate a latex chunk using the textsynth LLM api
+/// the preprompt is in the file "prompt.txt"
+/// the api key is in the file "api_key.txt" or
+/// in the environment variable "TEXTSYNTH_API_KEY"
+fn translate_one_chunk(chunk: &str, input_lang: &str, output_lang: &str) -> String {
+    // get the preprompt
+    let mut prompt = std::fs::read_to_string("src/prompt.txt").expect("cannot read preprompt");
+
+    let input_lang = get_lang_name(input_lang).to_string();
+    let output_lang = get_lang_name(output_lang).to_string();
+
+    // in the prompt, replace <lang_in> by the input language and <lang_out> by the output language
+    prompt = prompt.replace("<lang_in>", input_lang.as_str());
+    prompt = prompt.replace("<lang_out>", output_lang.as_str());
+
+    let question = format!("{}{}", prompt, chunk);
+    let trs_chunk = chat_with_ts(question.as_str());
+
     // remove the text before \begin{trsltx} and after \end{trsltx}
     let trs_chunk = trs_chunk.split("\\begin{trsltx}").collect::<Vec<&str>>()[1];
     let trs_chunk = trs_chunk.split("\\end{trsltx}").collect::<Vec<&str>>()[0];
     trs_chunk.to_string()
+}
+
+// test the chat_with_ts function
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_chat_with_ts() {
+        let question = "What is the capital of France?";
+        let answer = chat_with_ts(question);
+        println!("{:?}", answer);
+        assert!(answer.contains("Paris"));
+    }
+    #[test]
+    fn test_complete_grammar_ts() {
+        let question = "Q: la capitale de la France est-elle Paris? Répondre uniquement par oui ou non avec des caractères en minuscule.\nA:";
+        let grammar = 
+r#"root   ::= "oui" | "non""#;
+        let grammar = grammar.to_string();
+        println!("{:?}", grammar);
+        let answer = complete_with_ts(question, Some(grammar));
+        println!("{:?}", answer);
+    }
 }
